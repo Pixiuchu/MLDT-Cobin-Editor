49e5dfce21e0ef6be0126b71c1bcda8e7e733077
Snodeca <52331547+Snodeca@users.noreply.github.com>
2021-12-26 00:14:56 +0100

    initial commit

---
+++ MLDT Cobin Editor/Main Window.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace MLDT_Cobin_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numudMarioStartingBoots = new System.Windows.Forms.NumericUpDown();
+            this.openCodeBin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numudMarioStartingBoots)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Change Mario Boots";
+            // 
+            // numudMarioStartingBoots
+            // 
+            this.numudMarioStartingBoots.Location = new System.Drawing.Point(133, 137);
+            this.numudMarioStartingBoots.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numudMarioStartingBoots.Name = "numudMarioStartingBoots";
+            this.numudMarioStartingBoots.Size = new System.Drawing.Size(46, 23);
+            this.numudMarioStartingBoots.TabIndex = 1;
+            // 
+            // openCodeBin
+            // 
+            this.openCodeBin.Location = new System.Drawing.Point(13, 13);
+            this.openCodeBin.Name = "openCodeBin";
+            this.openCodeBin.Size = new System.Drawing.Size(126, 35);
+            this.openCodeBin.TabIndex = 2;
+            this.openCodeBin.Text = "Open Code.bin file";
+            this.openCodeBin.UseVisualStyleBackColor = true;
+            this.openCodeBin.Click += new System.EventHandler(this.openCodeBin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 349);
+            this.Controls.Add(this.openCodeBin);
+            this.Controls.Add(this.numudMarioStartingBoots);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MLDT Cobin Editor";
+            ((System.ComponentModel.ISupportInitialize)(this.numudMarioStartingBoots)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numudMarioStartingBoots;
+        private System.Windows.Forms.Button openCodeBin;
+    }
+}
+
---
+++ MLDT Cobin Editor/Main Window.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLDT_Cobin_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string savegame;
+
+        public static byte[] ML4E_StringToByteArray(string hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+
+        private void ML4E_get_openfile()
+        {
+            OpenFileDialog openFD = new OpenFileDialog();
+            if (openFD.ShowDialog() == DialogResult.OK)
+            {
+                savegame = openFD.FileName;
+            }
+        }
+
+        private void ML4E_get_data()
+        {
+            {
+                FileStream savegame_fs = new FileStream(savegame, FileMode.Open);
+                BinaryReader savegame_br = new BinaryReader(savegame_fs);
+                long length = savegame_fs.Length;
+
+                /* Mario Boots */
+                {   
+                    savegame_br.BaseStream.Position = 0x559002;
+                    byte mariostartingboots = (byte)savegame_fs.ReadByte();
+                    numudMarioStartingBoots.Text = numudMarioStartingBoots.ToString();
+                }
+            }
+        }
+
+        private void ML4E_set_data()
+        {
+            FileStream update_save_open = null;
+            BinaryWriter update_save_write = null;
+            update_save_open = new System.IO.FileStream(savegame, System.IO.FileMode.OpenOrCreate);
+            update_save_write = new System.IO.BinaryWriter(update_save_open);
+
+            #region data
+            byte[] mariostartingboots = ML4E_StringToByteArray(int.Parse(numudMarioStartingBoots.Text).ToString("X2"));
+            Array.Reverse(mariostartingboots);
+            update_save_open.Position = Convert.ToInt64("559002", 16);
+            update_save_write.Write(mariostartingboots);
+
+            #endregion
+            update_save_open.Close();
+        }
+        private void openCodeBin_Click(object sender, EventArgs e)
+        {
+            ML4E_get_openfile();
+            if (string.IsNullOrEmpty(savegame))
+            {
+                MessageBox.Show("No file selected");
+            }
+            else
+            {
+                ML4E_get_data();
+                FileStream savegame_fs = new FileStream(savegame, FileMode.Open);
+                BinaryReader savegame_br = new BinaryReader(savegame_fs);
+                long length = savegame_fs.Length;
+                savegame_br.BaseStream.Position = 0x77F;
+                byte[] rank = savegame_br.ReadBytes(4);
+                int rank1 = BitConverter.ToInt32(rank, 0); savegame_br.Close();
+            }
+        }
+
+    }
+}
