97223877626cab3a3267598a4abe51bfa91a1eee
Snodeca <52331547+Snodeca@users.noreply.github.com>
2021-12-26 17:58:31 +0100

    Changed layout

--- MLDT Cobin Editor/Main Window.Designer.cs
+++ MLDT Cobin Editor/Main Window.Designer.cs
@@ -29,7 +29,6 @@ namespace MLDT_Cobin_Editor
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.roMStartingBootsTXT = new System.Windows.Forms.TextBox();
             this.cmbbxMStartingBoots = new System.Windows.Forms.ComboBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -37,18 +36,14 @@ namespace MLDT_Cobin_Editor
             this.openCobin = new System.Windows.Forms.ToolStripMenuItem();
             this.saveCobin = new System.Windows.Forms.ToolStripMenuItem();
             this.roNumudMStartingBoots = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.roNumudMStartingBoots)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 139);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Change Mario Boots";
             // 
             // roMStartingBootsTXT
             // 
@@ -98,7 +93,7 @@ namespace MLDT_Cobin_Editor
             "Hiking Boots",
             "DoB Boots",
             "MINI Boots"});
-            this.cmbbxMStartingBoots.Location = new System.Drawing.Point(133, 136);
+            this.cmbbxMStartingBoots.Location = new System.Drawing.Point(109, 19);
             this.cmbbxMStartingBoots.MaxDropDownItems = 35;
             this.cmbbxMStartingBoots.Name = "cmbbxMStartingBoots";
             this.cmbbxMStartingBoots.Size = new System.Drawing.Size(121, 23);
@@ -107,11 +102,12 @@ namespace MLDT_Cobin_Editor
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1389, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1251, 24);
             this.menuStrip1.TabIndex = 6;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -127,7 +123,7 @@ namespace MLDT_Cobin_Editor
             // openCobin
             // 
             this.openCobin.Name = "openCobin";
-            this.openCobin.Size = new System.Drawing.Size(180, 22);
+            this.openCobin.Size = new System.Drawing.Size(103, 22);
             this.openCobin.Text = "Open";
             this.openCobin.Click += new System.EventHandler(this.openCobin_Click);
             // 
@@ -135,7 +131,7 @@ namespace MLDT_Cobin_Editor
             // 
             this.saveCobin.Enabled = false;
             this.saveCobin.Name = "saveCobin";
-            this.saveCobin.Size = new System.Drawing.Size(180, 22);
+            this.saveCobin.Size = new System.Drawing.Size(103, 22);
             this.saveCobin.Text = "Save";
             this.saveCobin.Click += new System.EventHandler(this.saveCobin_Click);
             // 
@@ -146,15 +142,57 @@ namespace MLDT_Cobin_Editor
             this.roNumudMStartingBoots.Size = new System.Drawing.Size(120, 23);
             this.roNumudMStartingBoots.TabIndex = 7;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Starting Boots";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Starting Gear";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Starting Hammer";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbbxMStartingBoots);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(241, 96);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mario\'s Starting Equipment";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1389, 673);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1251, 735);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.roNumudMStartingBoots);
-            this.Controls.Add(this.cmbbxMStartingBoots);
             this.Controls.Add(this.roMStartingBootsTXT);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -162,14 +200,14 @@ namespace MLDT_Cobin_Editor
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.roNumudMStartingBoots)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox roMStartingBootsTXT;
         private System.Windows.Forms.ComboBox cmbbxMStartingBoots;
         private System.Windows.Forms.MenuStrip menuStrip1;
@@ -177,6 +215,10 @@ namespace MLDT_Cobin_Editor
         private System.Windows.Forms.ToolStripMenuItem openCobin;
         private System.Windows.Forms.ToolStripMenuItem saveCobin;
         private System.Windows.Forms.NumericUpDown roNumudMStartingBoots;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
